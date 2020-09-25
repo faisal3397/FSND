eaf88ed800d082704eed465a76bc3e16a4c6126e
faisal3397 <faisal.alowaisi.97@gmail.com>
2020-09-25 18:54:32 +0300

    added missing functionalities in Models Artist&Venue, and Added Show Model

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -34,14 +34,14 @@ class Venue(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    genres = db.Column(db.String(500))
+    genres = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     website = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
-    seeking_description = db.Column(db.String)
+    seeking_description = db.Column(db.String(500))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
     past_shows_count = db.Column(db.Integer)
@@ -64,7 +64,16 @@ class Artist(db.Model):
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
 
+  id = db.Column(db.Integer, primary_key=True)
+  venue_id = db.Column(db.Integer)
+  venue_name = db.Column(db.String)
+  artist_id = db.Column(db.Integer)
+  artist_name = db.Column(db.String)
+  artist_image_link = db.Column(db.String(500))
+  start_time = db.Column(db.DateTime)
 #----------------------------------------------------------------------------#
 # Filters.
 #----------------------------------------------------------------------------#
