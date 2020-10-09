66d616820d04e7f866ec74bd00b6f18bc55618d2
faisal3397 <faisal.alowaisi.97@gmail.com>
2020-10-09 19:01:45 +0300

    implemented edit form endpoint [GET]

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -448,22 +448,33 @@ def edit_artist_submission(artist_id):
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
   form = VenueForm()
-  venue={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-  }
   # TODO: populate form with values from venue with ID <venue_id>
-  return render_template('forms/edit_venue.html', form=form, venue=venue)
+  venue = Venue.query.get(venue_id)
+
+  # since these fields are different, e.g. TextAreaField, BooleanField,etc..., 
+  # we need to handle it in a different way
+  seeking_talent = ''
+  if venue.seeking_talent:
+    form.seeking_talent.data = 'y'
+  form.seeking_description.data = venue.seeking_description
+  form.genres.data = venue.genres
+
+  data={
+    "id": venue.id,
+    "name": venue.name,
+    "address": venue.address,
+    "city": venue.city,
+    "state": venue.state,
+    "phone": venue.phone,
+    "website": venue.website,
+    "facebook_link": venue.facebook_link,
+    "image_link": venue.image_link,
+    "past_shows": venue.past_shows,
+    "upcoming_shows": venue.upcoming_shows,
+    "past_shows_count": len(venue.past_shows),
+    "upcoming_shows_count": len(venue.upcoming_shows),
+  }
+  return render_template('forms/edit_venue.html', form=form, venue=data)
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
