bc72c7c0a78c3d237847b9ae3cb3742d4173c816
faisal3397 <faisal.alowaisi.97@gmail.com>
2020-11-28 13:13:43 +0300

    implemented drinks-detail

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -22,23 +22,43 @@ db_drop_and_create_all()
 
 
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
 '''
 
 
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+    short_drinks = []
+    if len(drinks) > 0:
+        short_drinks = [drink.short() for drink in drinks]
+
+    return jsonify({
+        "success": True,
+        "drinks": short_drinks
+    })
+
+
 '''
-@TODO implement endpoint
+@DONE implement endpoint
     GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
 '''
+
+
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(jwt):
+    drinks = Drink.query.all()
+    long_drinks = []
+
+    if len(drinks) > 0:
+        long_drinks = [drink.long() for drink in drinks]
+
+    return jsonify({
+        "success": True,
+        "drinks": long_drinks
+    })
 
 
 '''
