700fa1c31bef5bb6959fa93ece1c729111db1a83
faisal3397 <faisal.alowaisi.97@gmail.com>
2020-11-29 16:22:04 +0300

    modified code to follow PEP8 style

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
@@ -13,11 +13,14 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 '''
+
+
 def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+
 
 '''
 db_drop_and_create_all()
@@ -25,14 +28,19 @@ db_drop_and_create_all()
     can be used to initialize a clean database
     !!NOTE you can change the database_filename variable to have multiple verisons of a database
 '''
+
+
 def db_drop_and_create_all():
     db.drop_all()
     db.create_all()
+
 
 '''
 Drink
 a persistent drink entity, extends the base SQLAlchemy Model
 '''
+
+
 class Drink(db.Model):
     # Autoincrementing, unique primary key
     id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
